3be51b928fb9398bd89569a6ba8ab136934e7f0c
angelo <angelodelperez@gmail.com>
2023-04-30 09:19:43 -0500

    AppSeguridad

---
+++ screens/Inicio.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+const Inicio = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Pagina Inicio</Text>
+    </View>
+  )
+}
+
+export default Inicio
